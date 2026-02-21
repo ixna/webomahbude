65ca72bfaa72cc812a59bed91a752a7d1e9aabfe
Ixna <isna11583@gmail.com>
2026-02-21 18:35:48 +0700

    Fix deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  output: "export",
+  basePath: "/webomahbude",
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
